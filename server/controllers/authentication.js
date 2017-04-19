118daf16aa200b38103ed0082d7128fac330a8c6
Brian Fajardo <bmf5@hotmail.com>
2017-04-19 18:33:53 -0400

    Handle sign up cases

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,3 +1,29 @@
+const User = require('../models/user')
+
 exports.signUp = (req, res, next) => {
-  res.send({ Working_in_postman: true })
+  const email = req.body.email
+  const password = req.body.password
+
+  // Search DB
+  User.findOne({ email }, (err, existingUser) => {
+    if (err) { return next(err) }
+
+    // Return error if email already used
+    if (existingUser) {
+      return res.status(422).send({ error: 'Email is already used' })
+    }
+
+    // If user with submitted email does not already exist, create and save
+    const user = new User({
+      email,
+      password
+    })
+
+    user.save(err => {
+      if (err) { return next(err) }
+
+      // Respond to request with created user
+      res.json({ success: true })
+    })
+  })
 }
